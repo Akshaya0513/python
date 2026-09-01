e54e569e9fed464d4dcb98bd907f2c0133b4eff0
Akshaya0513 <akshayarapolu0513@gmail.com>
2026-09-01 18:18:41 +0530

    Add files via upload

---
+++ Lecture1.py
@@ -0,0 +1,41 @@
+
+# type Conversation
+a = int ("2")
+b = 7.5
+print (type(a))
+print ( a + b)
+
+#Taking input 
+name = input ("Enter your name: ")
+print ("welcome", name)
+
+#Taking input
+name = input ("Enter your name: ")
+marks = float (input("Enter your marks: "))
+age = int (input ("Enter your age: "))
+print ("Welcome",name)
+print ("You scored ",marks)
+print ("Your age is ",age)
+
+#Question:1
+#Addition of two numbers
+a = int (input ("Enter a: "))
+b = int (input ("Enter b: "))
+print ("The sum = ",a +b)
+
+#Question: 2
+#Find the area of the square
+a = float(input ("Enter the side:"))
+print("The side of the square = ", a * a)
+
+#Questio: 3
+#Find the average of the two numbers
+a = float (input("Enter the first value: "))
+b = float (input("Enter the second value: "))
+print ("The average of the two numbers = ", (a + b) / 2)
+
+#Question: 4
+#To check wheathe a >= b
+a = int (input ("Enter the first number: "))
+b = int (input ("Enter the second number: "))
+print (a >= b)
